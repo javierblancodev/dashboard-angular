f6d3cc850a12341a66e5edff2b6337f6006fbe40
Javier Blanco <javierblancoit@gmail.com>
2022-07-25 10:11:41 +0100

    create shared module for shared components

---
+++ src/app/shared/divider/divider.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+// import { Input } from '@angular/core'; 
+
+@Component({
+  selector: 'app-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.css']
+})
+export class DividerComponent implements OnInit {
+
+  // @Input() title = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
