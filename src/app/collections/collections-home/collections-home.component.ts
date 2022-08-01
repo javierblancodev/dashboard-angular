eb7f8f49c8ca6af408d779bd98fa90c83f853d2c
Javier Blanco <javierblancoit@gmail.com>
2022-08-01 09:58:50 +0100

    create table component for the collections module

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: 'James', age: '24', job: 'Designer', employed: true },
+    { name: 'Jill', age: '26', job: 'Enginner', employed: false },
+    { name: 'Elyse', age: '25', job: 'Engineer', employed: true },
+    { name: 'Mark', age: '22', job: 'Developer', employed: false }
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Has a job?' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
